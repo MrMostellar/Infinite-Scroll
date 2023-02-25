17be7d0d37e145160217b62546cb81fbee7b48cf
Andrew M <mr.mostellar@gmail.com>
2023-02-24 19:43:20 -0700

    cleaned up code with a utility function

--- script.js
+++ script.js
@@ -5,21 +5,30 @@ const count = 10;
 const apiKey = 'a04JfAciLKIErx2hwDaw_nYjPP-4GJYwxoP6K3hhzEI';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`; 
 
+// utility function to set attributes
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 function displayPhotos(){
 
     // make a function that takes each object from the array and creates the "post"
     photosArray.forEach((photo) => {
         // for each photo create a hyperlink, title, and load from the source.
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
-
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
         // grab the src, alt text from the json file
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         // add the image and its attributes to the item variable, add the items variable to the container
         item.appendChild(img);
         contentContainer.appendChild(item);
